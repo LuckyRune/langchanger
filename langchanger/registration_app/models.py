9e3fe4a819a3145bcd50dacdc110d63e40db07db
GoRoSfan <olsngrs@gmail.com>
2020-03-21 20:26:39 +0200

    add str func / some field // change some field

--- langchanger/registration_app/models.py
+++ langchanger/registration_app/models.py
@@ -8,20 +8,28 @@ from translation_app.models import Origin
 # Create your models here.
 class UserProfile(models.Model):
 
-    rate = models.BigIntegerField('Общая оценка пользователя', )
-    description = models.TextField('О себе', max_length=1000)
+    rate = models.BigIntegerField('Общая оценка пользователя', default=0)
+    description = models.TextField('О себе', max_length=1000, blank=True, null=True)
 
     user = models.OneToOneField(User, verbose_name='Основной пользователь', on_delete=models.CASCADE)
-    achievements = models.ManyToManyField('Achievement', verbose_name='Достижения', blank=True, null=True)
-    on_hold = models.ManyToManyField(Origin, verbose_name='Отложенное', blank=True, null=True)
+    achievements = models.ManyToManyField('Achievement', verbose_name='Достижения', blank=True)
+    on_hold = models.ManyToManyField(Origin, verbose_name='Отложенное', blank=True)
 
-    profile_icon = models.ImageField('Аватарка', )
+    profile_icon = models.ImageField('Аватарка', upload_to='images/', blank=True, null=True)
+
+    def __str__(self):
+        user_label = "{} ({})".format(self.user.username, self.rate)
+        return user_label
 
 
 class Achievement(models.Model):
-    name = models.TextField('Название', max_length=30)
-    description = models.TextField('Описание', max_length=500)
-    rate = models.IntegerField('Стоимость', )
+    name = models.CharField('Название', max_length=30)
+    description = models.TextField('Описание', max_length=500, blank=True, null=True)
+    rate = models.IntegerField('Стоимость', default=1)
+
+    def __str__(self):
+        return self.name
 
     def add_rate(self, user: UserProfile):
         user.rate += self.rate
+
--- langchanger/translation_app/models.py
+++ langchanger/translation_app/models.py
@@ -1,70 +1,102 @@
 from django.db import models
 
-from registration_app.models import UserProfile
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Genre(models.Model):
-    name = models.TextField('Название жанра книги', max_length=30)
+    name = models.CharField('Название жанра произведения', max_length=30)
+
+    def __str__(self):
+        return self.name
 
 
 class FormatType(models.Model):
-    name = models.TextField('Название формата книги', max_length=30)
+    name = models.CharField('Название формата произведения', max_length=30)
+
+    def __str__(self):
+        return self.name
 
 
 class Language(models.Model):
-    name = models.TextField('Язык', max_length=30)
+    name = models.CharField('Язык', max_length=30)
+
+    def __str__(self):
+        return self.name
 
 
 class Origin(models.Model):
-    isbn = models.TextField('ISBN', max_length=13)
-    title = models.TextField('Название книги', max_length=50)
-    author = models.TextField('Автор', max_length=50)
-    description = models.TextField('Описание', max_length=1500)
+    isbn = models.CharField('ISBN', max_length=13)
+    title = models.CharField('Название книги', max_length=50)
+    author = models.CharField('Автор', max_length=50, blank=True, null=True)
+    description = models.TextField('Описание', max_length=1500, blank=True, null=True)
     creation_date = models.DateField('Дата создания', blank=True, null=True)
 
-    genre = models.ManyToManyField(Genre, verbose_name='Жанры', blank=True, null=True)
+    genre = models.ManyToManyField(Genre, verbose_name='Жанры', blank=True)
     origin_language = models.ForeignKey(Language, verbose_name='Язык оригинала', on_delete=models.SET_NULL,
-                                        blank=True, null=True
-                                        )
+                                        blank=True, null=True)
     format_type = models.ForeignKey(FormatType, verbose_name='Формат книги', on_delete=models.SET_NULL,
-                                    blank=True, null=True
-                                    )
+                                    blank=True, null=True)
 
-    AGES = ['0', '6', '12', '16', '18']
-    age_limit = models.TextField('Возрастной рейтинг', max_length=2, choices=AGES, default=AGES[-1])
+    AGES = [
+        ('0', '0'),
+        ('6', '6'),
+        ('12', '12'),
+        ('16', '16'),
+        ('18', '18')
+    ]
+    age_limit = models.CharField('Возрастной рейтинг', max_length=2, choices=AGES, default=AGES[-1])
 
-    poster = models.ImageField('Постер книги',)
-    source_link = models.FileField('Ссылка на книгу',)
+    poster = models.ImageField('Постер книги', upload_to='images/', blank=True, null=True)
+    source_link = models.FileField('Ссылка на книгу', upload_to='files/', blank=True, null=True)
+
+    def __str__(self):
+        origin_label = "{}, {}".format(self.title, self.author)
+        return origin_label
 
 
 class Translation(models.Model):
     creation_date = models.DateTimeField('Дата начала перевода', auto_now_add=True)
-    rate = models.BigIntegerField('Оценка',)
+    rate = models.BigIntegerField('Оценка', default=0)
 
-    author = models.ForeignKey(UserProfile, verbose_name='Автор', default='anonymous', on_delete=models.SET_DEFAULT)
+    author = models.ForeignKey(User, verbose_name='Автор', default=2, on_delete=models.SET_DEFAULT)
     origin = models.ForeignKey(Origin, verbose_name='Оригинал', on_delete=models.SET_NULL, blank=True, null=True)
     language = models.ForeignKey(Language, verbose_name='Язык', on_delete=models.SET_NULL, blank=True, null=True)
 
-    translation_link = models.FileField('Ссылка на перевод',)
+    translation_link = models.FileField('Ссылка на перевод', upload_to='files/', blank=True, null=True)
+
+    def __str__(self):
+        translation_label = "Автор перевода: {}; Язык: {}; Оригинал: {}".format(self.author, self.language, self.origin)
+        return translation_label
 
     def change_translation_link(self, version):
         pass
 
 
 class Version(models.Model):
+    change_number = models.PositiveIntegerField('Число изменений', default=0)
     creation_date = models.DateTimeField('Дата создания', auto_now_add=True)
 
     translation = models.ForeignKey(Translation, verbose_name='Основной перевод', on_delete=models.CASCADE)
 
-    version_link = models.FileField('Ссылка на версию', )
+    version_link = models.FileField('Ссылка на версию', upload_to='files/', blank=True, null=True)
+
+    def __str__(self):
+        translation = self.translation.__str__()
+        version_label = "{} № {} ({})".format(translation, self.change_number, self.creation_date)
+        return version_label
 
 
 class RateList(models.Model):
-    rate = models.SmallIntegerField('Оценка', )
+    rate = models.SmallIntegerField('Оценка', default=0)
 
-    user = models.ForeignKey(UserProfile, verbose_name='Пользователь', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
     translation = models.ForeignKey(Translation, verbose_name='Перевод', on_delete=models.CASCADE)
+
+    def __str__(self):
+        translation = self.translation.__str__()
+        rate_label = "Пользователь {} поставил{} переводу: {}".format(self.user.username, self.rate, translation)
+        return rate_label
 
     def add_rate(self):
         pass
@@ -80,12 +112,20 @@ class Comment(models.Model):
     post = models.TextField('Текст комментария', max_length=1000)
     post_date = models.DateTimeField('Дата создания', auto_now_add=True)
 
-    author = models.ForeignKey(UserProfile, verbose_name='Автор', default='anonymous', on_delete=models.SET_DEFAULT)
+    author = models.ForeignKey(User, verbose_name='Автор', default=2, on_delete=models.SET_DEFAULT)
     parent_comment = models.ForeignKey('Comment', verbose_name='Родительский коментарий', on_delete=models.CASCADE,
-                                       null=True, blank=True
-                                       )
+                                       null=True, blank=True)
+
+    def __str__(self):
+        comment_label = "{}: {}...".format(self.author.username, self.post[:20])
+        return comment_label
 
 
 class CommentOrigin(models.Model):
     comment = models.OneToOneField(Comment, verbose_name='Основной комментарий', on_delete=models.CASCADE)
     origin = models.ForeignKey(Origin, verbose_name='Книга', on_delete=models.CASCADE)
+
+    def __str__(self):
+        comment = self.comment.__str__()
+        origin = self.origin.__str__()
+        return "{} ({})".format(comment, origin)
