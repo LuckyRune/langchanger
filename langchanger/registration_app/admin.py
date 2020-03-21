346ed9b47ed50d47b5bbefdd2bac1eb53adfc260
GoRoSfan <olsngrs@gmail.com>
2020-03-21 20:27:25 +0200

    set default admin panel

--- langchanger/registration_app/admin.py
+++ langchanger/registration_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import *
+
+
 # Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    fields = ('description', 'user', 'profile_icon')
+
+
+@admin.register(Achievement)
+class AchievementAdmin(admin.ModelAdmin):
+    fields = ('name', 'description', 'rate')
--- langchanger/translation_app/admin.py
+++ langchanger/translation_app/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+admin.site.register(Genre)
+admin.site.register(FormatType)
+admin.site.register(Language)
+
+
+@admin.register(Origin)
+class OriginAdmin(admin.ModelAdmin):
+    fields = ('isbn', 'title', 'author', 'description', 'creation_date',
+              'genre', ('origin_language', 'format_type'), 'age_limit',
+              'poster', 'source_link')
+
+
+@admin.register(Translation)
+class TranslationAdmin(admin.ModelAdmin):
+    fields = ('author', 'origin', 'language')
+
+
+@admin.register(RateList)
+class RateListAdmin(admin.ModelAdmin):
+    fields = ('rate', 'user', 'translation')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    fields = ('post', 'author', 'parent_comment')
+
+
+@admin.register(CommentOrigin)
+class CommentOriginAdmin(admin.ModelAdmin):
+    fields = ('comment', 'origin')
