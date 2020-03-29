c6eb224d9ddfbf4d02c88e33f7b0652b5cf48dc5
GoRoSfan <olsngrs@gmail.com>
2020-03-29 20:42:59 +0300

    make draft views

--- langchanger/registration_app/views.py
+++ langchanger/registration_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -7,6 +7,20 @@ from rest_framework.renderers import JSONRenderer
 
 from .models import *
 from .serializers import *
+from translation_app.models import Translation, Origin
+from translation_app.serializers import UserProfileTranslationSerializer, MainInfoOriginSerializer
+
+
+def paginator(request, queryset):
+    page_size = int(request.GET.get('page_size', 3))
+    current_page = int(request.GET.get('current_page', 1))
+
+    first_item = (current_page - 1) * page_size
+    last_item = current_page * page_size
+
+    queryset_part = queryset[first_item:last_item]
+
+    return queryset_part
 
 
 class AllUserView(APIView):
@@ -15,7 +29,17 @@ class AllUserView(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        pass
+        queryset = UserProfile.objects.all()
+
+        response_queryset = paginator(request, queryset)
+
+        serializer = AllUserProfileSerializer(response_queryset, many=True)
+        for user in serializer:
+            user['field'] = '+'
+
+        content = {'data': serializer.data}
+
+        return Response(content)
 
 
 class ProfileUserView(APIView):
@@ -24,7 +48,20 @@ class ProfileUserView(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        pass
+        pk = int(request.GET.get('user', -1))
+
+        user = get_object_or_404(UserProfile, pk=pk)
+        translations = Translation.objects.filter(user=pk)
+
+        serializer_translations = UserProfileTranslationSerializer(translations, many=True)
+        serializer_user = UserProfileSerializer(user)
+
+        content = {'data': {
+            'user': serializer_user.data,
+            'translations': serializer_translations.data,
+        }}
+
+        return Response(content)
 
 
 class AchievementUserView(APIView):
@@ -33,7 +70,16 @@ class AchievementUserView(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        pass
+        user = int(request.GET.get('user', -1))
+
+        achievements_id = get_object_or_404(UserProfile, pk=user).objects.values('achievements')
+        achievements_object = Achievement.objects.filter(id__in=achievements_id)
+
+        serializer = AchievementSerializer(achievements_object, many=True)
+
+        content = {'data': serializer.data}
+
+        return Response(content)
 
 
 class HoldOnUserView(APIView):
@@ -42,13 +88,30 @@ class HoldOnUserView(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        pass
+        user = int(request.GET.get('user', -1))
+
+        origins_id = get_object_or_404(UserProfile, pk=user).objects.values('hold_on')
+        origins_object = Origin.objects.filter(id__in=origins_id)
+
+        serializer = MainInfoOriginSerializer(origins_object, many=True)
+
+        content = {'data': serializer.data}
+
+        return Response(content)
 
 
 class SettingUserView(APIView):
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
 
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        pass
+        pk = int(request.GET.get('user', -1))
+
+        user = get_object_or_404(UserProfile, pk=pk)
+
+        serializer = SettingUserSerializer(user, many=True)
+
+        content = {'data': serializer.data}
+
+        return Response(content)
--- langchanger/translation_app/views.py
+++ langchanger/translation_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404
+from django.db.models import Q
+from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -7,6 +9,19 @@ from rest_framework.renderers import JSONRenderer
 
 from .models import *
 from .serializers import *
+from . import __package__ as app
+
+
+def paginator(request, queryset):
+    page_size = int(request.GET.get('page_size', 3))
+    current_page = int(request.GET.get('current_page', 1))
+
+    first_item = (current_page - 1) * page_size
+    last_item = current_page * page_size
+
+    queryset_part = queryset[first_item:last_item]
+
+    return queryset_part
 
 
 class MainPageView(APIView):
@@ -15,7 +30,12 @@ class MainPageView(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        pass
+        last_origins = Origin.objects.all()[:6]
+        serializer = MainInfoOriginSerializer(last_origins, many=True)
+
+        content = {'data': serializer.data}
+
+        return Response(content)
 
 
 class AllOriginView(APIView):
@@ -24,7 +44,23 @@ class AllOriginView(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        pass
+        filter_dict = request.GET.get('filters', None)
+
+        if filter_dict is None:
+            queryset = Origin.objects.all()
+        else:
+            queryset = Origin.objects.filter(
+                format_type__name__in=filter_dict['format_type'],
+                genre__name__in=filter_dict['genre'],
+                age_limit__in=filter_dict['age_limit'])
+
+        response_queryset = paginator(request, queryset)
+
+        serializer = AllOriginSerializer(response_queryset, many=True)
+
+        content = {'data': serializer.data}
+
+        return Response(content)
 
 
 class OneOriginView(APIView):
@@ -33,16 +69,21 @@ class OneOriginView(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        pass
+        pk = int(request.GET.get('origin', -1))
 
+        origin = get_object_or_404(Origin, pk=pk)
+        translation_lang = Translation.objects.filter(origin=pk).only('language').distinct()
+        languages = Language.objects.filter(pk__in=translation_lang)
 
-class ReadOriginView(APIView):
-    permission_classes = [permissions.AllowAny]
+        serializer_language = LanguageSerializer(languages, many=True)
+        serializer_origin = OneOriginSerializer(origin, many=True)
 
-    renderer_classes = [JSONRenderer]
+        content = {'data': {
+            'origin': serializer_origin.data,
+            'languages': serializer_language.data,
+        }}
 
-    def get(self, request):
-        pass
+        return Response(content)
 
 
 class TranslationByLanguageView(APIView):
@@ -51,7 +92,38 @@ class TranslationByLanguageView(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        pass
+        origin = int(request.GET.get('origin', -1))
+        language = request.GET.get('language', None)
+
+        if origin != -1 and language is not None:
+            translations = Translation.objects.filter(
+                Q(origin=origin) &
+                Q(language__name=language)
+            )
+        else:
+            raise Http404
+
+        serializer = OriginTranslationSerializer(translations, many=True)
+
+        content = {'data': serializer.data}
+
+        return Response(content)
+
+
+class ReadOriginView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    renderer_classes = [JSONRenderer]
+
+    def get(self, request):
+        pk = int(request.GET.get('origin', -1))
+
+        origin = get_object_or_404(Origin, pk=pk)
+        serializer = ReadOriginSerializer(origin, many=True)
+
+        content = {'data': serializer.data}
+
+        return Response(content)
 
 
 class ReadTranslationView(APIView):
@@ -60,16 +132,45 @@ class ReadTranslationView(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        pass
+        pk = int(request.GET.get('translation', -1))
+
+        translation = get_object_or_404(Translation, pk=pk)
+        last_version = get_list_or_404(Version, translation=pk).objects.latest('creation_date')
+
+        serializer_translation = ReadTranslationSerializer(translation)
+        serializer_version = ReadVersionSerializer(last_version)
+
+        content = {'data': {
+            'translation': serializer_translation.data,
+            'last_version': serializer_version,
+        }}
+
+        return Response(content)
 
 
 class MakeTranslationView(APIView):
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
 
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        pass
+        pk = int(request.GET.get('translation', -1))
+
+        if pk != -1:
+            translation = Translation.objects.get(pk=pk)
+            last_version = Version.objects.filter(translation=pk).latest('creation_date')
+
+            serializer_translation = ReadTranslationSerializer(translation)
+            serializer_version = ReadVersionSerializer(last_version)
+
+            content = {'data': {
+                'translation': serializer_translation.data,
+                'last_version': serializer_version,
+            }}
+        else:
+            content = {'data': 'None'}
+
+        return Response(content)
 
 
 class AllVersionView(APIView):
@@ -78,7 +179,14 @@ class AllVersionView(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        pass
+        translation = int(request.GET.get('translation', -1))
+
+        versions = get_list_or_404(Origin, translation=translation)
+        serializer = AllVersionSerializer(versions, many=True)
+
+        content = {'data':  serializer.data}
+
+        return Response(content)
 
 
 class DifferenceVersionView(APIView):
@@ -87,4 +195,18 @@ class DifferenceVersionView(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        pass
+        pk = int(request.GET.get('version', -1))
+
+        current_version = get_object_or_404(Version, pk=pk)
+        translation = current_version.translation
+        last_version = Version.objects.filter(translation=translation).latest('creation_date')
+
+        serializer_current = ReadVersionSerializer(current_version, many=True)
+        serializer_last = ReadVersionSerializer(last_version, many=True)
+
+        content = {'data': {
+            'current_version': serializer_current.data,
+            'last_version': serializer_last.data,
+        }}
+
+        return Response(content)
