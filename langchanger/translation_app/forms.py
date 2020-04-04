e72e174d05e8a89e1d3229612552c9093930210d
GoRoSfan <olsngrs@gmail.com>
2020-04-04 19:09:54 +0300

    define draft classes

---
+++ langchanger/translation_app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import *
+
+
+# *
+class CreateRateForm(forms.ModelForm):
+    pass
+
+
+# *
+class ChangeRateForm(forms.ModelForm):
+    pass
