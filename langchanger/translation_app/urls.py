ee47a58bf0344cbd35149db0185e286cf3187e7c
GoRoSfan <olsngrs@gmail.com>
2020-04-09 19:54:28 +0300

    refactoring / add post url

--- langchanger/translation_app/urls.py
+++ langchanger/translation_app/urls.py
@@ -5,16 +5,22 @@ from . import views
 
 urlpatterns = [
     url(r'^main/$', views.MainPageView.as_view(), name='main-page'),
-    url(r'^all-origin/$', views.AllOriginView.as_view(), name='all-origin'),
+    url(r'^origin/all/$', views.AllOriginView.as_view(), name='all-origin'),
     url(r'^origin/$', views.OneOriginView.as_view()),
-    url(r'^origin-translation/$', views.TranslationByLanguageView.as_view()),
-    url(r'^read-origin/$', views.ReadOriginView.as_view()),
+    url(r'^origin/translation/$', views.TranslationByLanguageView.as_view()),
+    url(r'^origin/read/$', views.ReadOriginView.as_view()),
 
-    url(r'^read-translation/$', views.ReadTranslationView.as_view()),
-    url(r'^make-translation/$', views.MakeTranslationView.as_view()),
+    url(r'^translation/read/$', views.ReadTranslationView.as_view()),
+    url(r'^translation/add/$', views.MakeTranslationView.as_view()),
+    url(r'^translation/add_version/$', views.MakeTranslationView.as_view()),
+    url(r'^translation/delete/$', views.MakeTranslationView.as_view()),
 
-    url(r'^all-version/$', views.AllVersionView.as_view()),
-    url(r'^version-differences/$', views.DifferencesVersionView.as_view()),
+    url(r'^version/all/$', views.AllVersionView.as_view()),
+    url(r'^version/delete/$', views.DeleteVersionView.as_view()),
+    url(r'^version/differences/$', views.DifferencesVersionView.as_view()),
 
     url(r'^comment-origin/$', views.OriginCommentView.as_view()),
+    url(r'^comment-origin/add/$', views.MakeOriginCommentView.as_view()),
+
+    url(r'^rate/add/$', views.MakeRateView.as_view()),
 ]
