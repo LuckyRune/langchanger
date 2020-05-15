42e36c0464ac2a7b5add64a8a50db433dc776560
GoRoSfan <olsngrs@gmail.com>
2020-05-15 20:51:26 +0300

    add delete user view

--- langchanger/registration_app/urls.py
+++ langchanger/registration_app/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     url(r'^profile/$', views.ProfileUserView.as_view(), name='user-profile'),
     url(r'^achievement/$', views.AchievementView.as_view(), name='achievements'),
     url(r'^on-hold/$', views.OnHoldUserView.as_view(), name='user-on-hold'),
+
     url(r'^setting/$', views.SettingUserView.as_view(), name='user-setting'),
+    url(r'^delete/$', views.SettingUserView.as_view(), name='user-delete'),
 
     url(r'^register/$', views.RegisterUserView.as_view(), name='registration'),
 
