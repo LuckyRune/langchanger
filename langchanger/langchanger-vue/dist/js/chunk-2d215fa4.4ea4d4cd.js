9add95ada39e1e072def7a61e8033425419ac0c9
Andrey Fedorov <arthes.menetil@gmail.com>
2020-05-27 16:41:34 +0300

    build

---
+++ langchanger/langchanger-vue/dist/js/chunk-2d215fa4.4ea4d4cd.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d215fa4"],{c100:function(t,n,e){"use strict";e.r(n);var u=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div")},c=[],o={name:"Logout",created:function(){var t=this;this.$store.dispatch("DESTROY_TOKEN").then((function(n){t.$router.push("/")})).catch((function(n){t.$router.push("/")}))}},r=o,s=e("2877"),a=Object(s["a"])(r,u,c,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d215fa4.4ea4d4cd.js.map
